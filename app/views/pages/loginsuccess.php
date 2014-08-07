7826c6c94e4e9e7e1f73e775d1f2279469485173
Tai Travis <tt@pingv.com>
2014-08-07 08:53:42 -0700

    finished converting from 12 to 16 grud

--- app/views/pages/loginsuccess.php
+++ app/views/pages/loginsuccess.php
@@ -1,6 +1,6 @@
     <div class="row">
-      <div class="col-md-6 col-md-offset-3">
-        
+      <div class="col-md-8 col-md-offset-4">
+
         <h3>Login was successfull!</h3>
 
         <p>Go to <?php echo anchor('/allpatterns', 'All Patterns'); ?> or <?php echo anchor('/guide', 'All Documentation'); ?></p>
